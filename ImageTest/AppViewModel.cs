783f2d3abf8da922f0692d297bb038225f032dce
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-05-22 23:24:19 +0900

    viewModelを利用してデータを共通化

---
+++ ImageTest/AppViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Formats.Tar;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ImageTest
+{
+    public class AppViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        private string test = "";
+
+        public TestCommand TestCommand { get; set; }
+
+        public AppViewModel()
+        {
+            TestCommand = new TestCommand(this);
+        }
+
+        public string Value
+        {
+            get
+            {
+                return test;
+            }
+
+            set
+            {
+                test = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(
+                        this,
+                        new PropertyChangedEventArgs("Value")
+                    );
+                }
+            }
+        }
+    }
+}
